99f001fd1cb03eebba07a33e62967af3bf12bdcf
Devin <ho.devin05@gmail.com>
2015-05-26 10:53:53 -0700

    quick update

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import urlparse
 
-ADDR = "45.55.212.169" #45.55.212.169
+ADDR = "localhost" #45.55.212.169
 PORT = 8000
 
 class RequestHandler(BaseHTTPRequestHandler):        
@@ -25,10 +25,29 @@ class RequestHandler(BaseHTTPRequestHandler):
 
         s.wfile.write("<body>")
         for key, value in post_data.iteritems():
-        	if key == "body":
-    			s.wfile.write("<p>%s=%s</p>"  % (key, value))
-    			print "%s=%s , " % (key, value)
+        	if key == "Body":
+        		body = ''.join(value).encode('ascii', 'ignore')
 
+        		position = body.find(" ")
+
+        		if position == -1:
+        			command = body
+        		else:
+        			command = body[0:position]
+        			remainder = body[position:len(value)]
+
+        		if command == "menu":
+        			print "The available commands are:\r\new - create a new ticket\r\n"
+        			print "[id] - update this ticket ID\r\n"
+        			print "menu - show this menu"
+        		elif command == "new":
+        			print "new"
+        		elif command.isdigit():
+        			print "isdigit"
+        		else:
+        			print "Hello and thanks for the message. Unfortunately I did not quite understand what you needed."
+    			#s.wfile.write("<p>%s=%s</p>"  % (key, value))
+    			#print "%s=%s , " % (key, value)
         s.wfile.write("</body>")
 
        

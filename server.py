8db4f39452500dbf3534f5c7a4bf0641652b1e39
Devin <ho.devin05@gmail.com>
2015-05-26 12:15:43 -0700

    add create new ticket

--- server.py
+++ server.py
@@ -2,16 +2,38 @@
 
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 import urlparse
+import json
+import requests
 
-ADDR = "localhost" #45.55.212.169
+ADDR = '45.55.212.169' #45.55.212.169
 PORT = 8000
+
+def createTicket(data):
+    subject = 'SUBJECT'
+    body = 'BODY'
+    data = {'ticket': {'subject': subject, 'comment':{'body': body}, 'fields': {'25897847':'test'}}}
+
+    payload = json.dumps(data)
+
+    url = 'https://curbsidehelp.zendesk.com/api/v2/tickets.json'
+    user = 'ho.devin05@gmail.com'
+    pwd = '7ib18a8erhh'
+    headers = {'content-type': 'application/json'}
+
+    r = requests.post(url, data=payload, auth=(user,pwd), headers=headers)
+
+    if r.status_code != 201:
+        print('Status:', response.status_code, 'Problem with the request. Exiting.')    
+        exit()
+
+    print('Successfully created the ticket.')
 
 class RequestHandler(BaseHTTPRequestHandler):        
     def do_GET(s):
         s.send_response(200)
-        s.send_header("Content-type", "text/html")
+        s.send_header('Content-type', 'text/html')
         s.end_headers()
-        s.wfile.write("<body><p>This is a test.</p>")
+        s.wfile.write('<body><p>Success</p></body>')
 
     def do_POST(s):
     	length = int(s.headers['Content-Length'])
@@ -20,15 +42,15 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 
     	s.send_response(200)
-        s.send_header("Content-type", "text/html")
+        s.send_header('Content-type', 'text/html')
         s.end_headers()
 
-        s.wfile.write("<body>")
+        s.wfile.write('<body> <p>Success</p>')
         for key, value in post_data.iteritems():
-        	if key == "Body":
+        	if key == 'Body':
         		body = ''.join(value).encode('ascii', 'ignore')
 
-        		position = body.find(" ")
+        		position = body.find(' ')
 
         		if position == -1:
         			command = body
@@ -36,23 +58,23 @@ class RequestHandler(BaseHTTPRequestHandler):
         			command = body[0:position]
         			remainder = body[position:len(value)]
 
-        		if command == "menu":
-        			print "The available commands are:\r\new - create a new ticket\r\n"
-        			print "[id] - update this ticket ID\r\n"
-        			print "menu - show this menu"
-        		elif command == "new":
-        			print "new"
-        		elif command.isdigit():
-        			print "isdigit"
-        		else:
-        			print "Hello and thanks for the message. Unfortunately I did not quite understand what you needed."
-    			#s.wfile.write("<p>%s=%s</p>"  % (key, value))
-    			#print "%s=%s , " % (key, value)
-        s.wfile.write("</body>")
-
-       
-
-    	
+                if command == 'menu':
+                    print '\nThe available commands are:\r\n'
+                    print 'new - create a new ticket\r'
+                    print '[id] - update this ticket ID\r'
+                    print 'menu - show this menu\r\n'
+                elif command == 'new':
+                    #createTicket(remainder)
+                    print post_data
+                elif command.isdigit():
+                    print 'isdigit'
+                else:
+                    print '\nHello and thanks for the message.\r'
+                    print 'Unfortunately I did not quite understand what you needed.\r'
+                    print 'Try sending the word "menu" for the list of commands.\r\n'
+                # s.wfile.write('<p>%s=%s</p>'  % (key, value))
+                # print '%s=%s , ' % (key, value)
+        s.wfile.write('</body>')
 
 
 httpd = HTTPServer((ADDR, PORT), RequestHandler)

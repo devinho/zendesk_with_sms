4229057f504bfc8f3f3ee0f5720150d22befbe51
Devin <ho.devin05@gmail.com>
2015-05-29 12:10:00 -0700

    settings

--- server.py
+++ server.py
@@ -5,17 +5,8 @@ import requests
 from twilio.rest import TwilioRestClient 
 import os
 
-# from settings import *
+from settings import *
 
-ACCOUNT_SID = "AC054da9e69b02dc42dc306cd9ce60a7f9" 
-AUTH_TOKEN = "a87a3720a186d2fdfdf7f9074fec7344" 
-twilio_phone = '+14089139034'
-
-# zendesk
-user = os.getenv('email')
-pwd = os.getenv('pass')
-zendesk_url = 'https://curbsidehelp.zendesk.com'
-phone_field_id = 25897847 
 
 # future work/things to consider:
 # 1. notififying user of comment on their ticket needs work 
